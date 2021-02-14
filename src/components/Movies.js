8f765e1ea66b2fa42d3829175d3425379c53191e
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-02-14 09:49:00 -0600

    Done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const renderMovies = () => {
+    return movies.map(movie=>(
+      <div>
+        <h3>Name: {movie.title}</h3>
+        Time: {movie.time}
+        <br></br>
+        <br></br>
+        Genres:
+        <br></br>
+        <ul>
+          {movie.genres.map(genre=><li>{genre}</li>)}
+        </ul>
+      </div>
+    ))
+  }
+  
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
